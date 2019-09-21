f0fedd4e17f5aa86a35f8dfc1ed567f1019f72e0
ecisneros24 <elthoncisneros01@gmail,.com>
2019-09-21 11:15:24 -0700

    Modified the default marker to CSUDH coordinates.
    Things to do next:
    1. Have camera of Maps zoom into a closer view of CSUDH by default.
    2. Add search bar

--- app/src/main/java/com/example/googlemapstest0918/MapsActivity.java
+++ app/src/main/java/com/example/googlemapstest0918/MapsActivity.java
@@ -39,9 +39,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Sydney and move the camera //CHANGED TO CSUDH
+        LatLng csudh = new LatLng(33.8647829, -118.2552142);
+        //LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(csudh).title("Marker in CSUDH"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(csudh));
     }
 }
