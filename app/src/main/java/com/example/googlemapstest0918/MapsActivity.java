5c1f9b9d0f9d3bb779a60b29b6359f95685c6b4f
ecisneros24 <elthoncisneros01@gmail,.com>
2019-09-23 19:40:03 -0700

    Changelog 09/23/2019 (7:36PM):
    1.) Was able to get search bar working (using Google Geocoder). User can now enter search queries like "csudh lot 8" and a marker will appear on map. Geocoder takes the user input string and searches for the GPS coordinates of the address/place they are looking for. Then we take those geocoder-generated GPS coordinates and place a marker on the map. We then zoom into that marker. Need to now implement google maps Directions API for navigation WITHIN app, not switching to google maps app.
    
    2.) Gave searchView element an android id of: search_bar
    
    Changelog from previously committed build (09/23/2019 1:00pm).
    1. Changed Maps Activity from showing only a Google maps fragment to a RelativeLayout view showing Maps fragment and SearchView searchbar (still need to add functionality to searchbar. Searchbar reads "Where to next, Toro?".
    
    2. Changed Map Type from Normal to Hybrid view (like a satellite view)
    
    3. Used 'newLatLngZoom' method to zoom into CSUDH coordinates at Level 16 by default/app launch.
    
    To do next:
    
    1. Integrate Google Maps Directions API
    2. Send input from the SearchView to Google Maps Directions API to calculate best directions
    3. Store some test CSUDH buildings to test navigation.
    4. Add button for user to locate themselves w/ current GPS location.

--- app/src/main/java/com/example/googlemapstest0918/MapsActivity.java
+++ app/src/main/java/com/example/googlemapstest0918/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.example.googlemapstest0918;
-
+import java.util.List;
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Camera;
+import android.location.Geocoder; // Geocoder is for transforming address --> GPS coordinates (could be used for planting markers on maps for navigation)
+import android.location.Address;
 import android.os.Bundle;
 
+import android.widget.SearchView; //imported this for SearchView widget support; Website for help on searchviews: https://abhiandroid.com/ui/searchview
+
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +17,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+import java.io.IOException;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    SearchView searchView; //instanciating Searchview object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        searchView = findViewById(R.id.search_bar); // searchview object is being by our "search_bar search view we created in our activity_maps.xml file
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String location = searchView.getQuery().toString(); //this grabs user-entered text from SearchView search box; saves as string
+                List<Address> addressList = null;
+
+                if(location != null || !location.equals("")){ //makes sure there is no invalid user entry
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+                    try{
+                        addressList = geocoder.getFromLocationName(location, 1); // Google Geocoder converts user-entered text to GPS coordinates (we will use to place marker on map and (later) enable our own navigation
+                        //Google Geocoder documentation here: https://developer.android.com/reference/android/location/Geocoder
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                return false;
+            }
+            //********CITATION*** The code relating to searchView above was derived from the following YouTube video: https://www.youtube.com/watch?v=iWYsBDCGhGw
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+
         mapFragment.getMapAsync(this);
     }
 
